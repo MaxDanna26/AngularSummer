401bf07d2a629d2ee3de917a1ee01acb5c53f319
MaxDanna26 <maximilianodanna@hotmail.com>
2024-07-05 17:12:50 +0200

    Adding some ex

---
+++ Js/Ejercicio2/js2.js
@@ -0,0 +1,5 @@
+let numberOne = 2;
+let numberTwo = 2;
+let resultado = numberOne + numberTwo;
+
+alert("El resulado de la suma aleatoria es : " + resultado);
---
+++ Js/Ejercicio3/js3.js
@@ -0,0 +1,3 @@
+let mensaje = "<javascript> Hola mundo! Que facil es incluir \'comillas simples\' y \"comillas dobles!\"";
+
+alert(mensaje);ñ
